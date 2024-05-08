f12d0aa6793183bfcb59cf36dc4b5f18842eba2a
FelSec <felsec@protonmail.com>
2024-05-08 21:14:02 +0100

    Added threading to the solution

--- noteapi.py
+++ noteapi.py
@@ -1,6 +1,4 @@
 from note import note
 
 if __name__ == '__main__':
-    #from waitress import serve
-    #serve(note, port="80")
-    note.run(debug=False, port="80")
+    note.run(debug=False, port="80", threaded=True)
